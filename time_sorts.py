387d2aebb0be3c107c2677317b0aa6ce14c345b5
Jeff Silv erman <jeffsilverm@gmail.com>
2013-11-28 22:16:00 -0800

    Added some internal documentation to make the code easier to understand

--- time_sorts.py
+++ time_sorts.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 #
-#
+# This program times the merge sort and the bubble sort for various values of
+# N, where N is the size of a list of integers to be sorted.  N is passed as
+# an argument on the command line.
 import bubble_sort
 import merge_sort
 import random
@@ -26,6 +28,11 @@ def call_merge_sort(N) :
 if __name__ == '__main__':
     import timeit
     N = int(sys.argv[1])
+# This is a little pythonic trickery.  The input to the timeit.timeit method is
+# a snippet of code, which gets executed number of times.  In order to
+# parameterize the code, use string substitution, the % operator, to modify the
+# string.  Note that this code has to import itself in order to get the
+# subroutines in scope.
     print("Bubble Sort:", timeit.timeit("time_sorts.call_bubble_sort(%d)" % N ,
           setup="import time_sorts", number=10000))
     print("Merge Sort:", timeit.timeit("time_sorts.call_merge_sort(%d)" % N ,
